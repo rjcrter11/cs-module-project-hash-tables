50fd33fee110b6b8d1a1de76b5d511a95d991694
Rachel <rjcrter11@gmail.com>
2020-07-08 16:32:10 -0500

    tests pass for 3 of applications files

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,11 @@
 # Your code here
+import math
+import random
+
+# Build a lookup table
+# Build off the first instance of v
+# If its not in the table, copy slow function
+# if it already is, return the lookup table
 
 
 def slowfun_too_slow(x, y):
@@ -9,17 +16,26 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+lookup_table = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
+    v = math.pow(x, y)
+    if v not in lookup_table:
+        lookup_table[v] = math.factorial(v)
+        lookup_table[v] //= (x + y)
+        lookup_table[v] %= 982451653
+        return lookup_table[v]
+    return lookup_table[v]
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,30 @@
-def word_count(s):
-    # Your code here
+# lowercase all letters
+# split on whitespace
+# iterate through string to remove unwanted characters
+# iterate through that to add to counter
+# return dictionary with count of how many times a word occurs
 
+def word_count(s):
+    bad_chars = ['"', ':', ';', ',', '.', '-', '+', '=', '/',
+                 '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    counter = {}
+    s = s.lower()
+    for i in bad_chars:
+        s = s.replace(i, '')
+    res = s.split()
+
+    for word in res:
+        if word not in counter:
+            counter[word] = 1
+        else:
+            counter[word] += 1
+
+    return counter
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
